22bac2b9650ae527c2f8ce08548d8d33cdfc9260
Justin Zimmerman <justin@zimmland.com>
2026-05-09 08:01:05 -0700

    Round trip SPI is working.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
+#include "stm32f4xx.h"
 #include "spi.h"
 #include "encoder.h"
 
-int main(void)
-{
-    return 1;
+int main(void) {
+    telem_buf[1].pos_cmd      = 0x12345678;
+    telem_buf[1].pos_fbk      = 0x87654321;
+    telem_buf[1].timestamp_ms = 0xDEADBEEF;
+    telem_buf[1].drive_state  = DRIVE_IDLE;
+    telem_write_idx = 0;
+
+    spi_init();
+    encoder_init();
+
+    while (1) {}
 }
