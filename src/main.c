3a7726859407c50e3b4bcfd3f6e04fca00fc9916
Justin Zimmerman <justin@zimmland.com>
2026-05-22 10:54:23 -0700

    feat: main.c — clock_init, drive_init, spi_init, SysTick 1kHz, FPU enabled

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "stm32f4xx.h"
 #include <string.h>
 #include "clock.h"
+#include "drive.h"
 
 extern volatile uint32_t cnt_data;
 extern volatile uint32_t cnt_error;
@@ -11,6 +12,7 @@ extern volatile uint8_t  dbg_rx0;
 int main(void)
 {
     clock_init();
+    drive_init();
     spi_init();
 
     while (1) {}
